2829b2d50c202526319cd37e57b62ba09f2c447a
VAxoJan <vaxojaniashvili186@gmail.com>
2023-11-30 17:57:47 +0400

    ds

--- javaScript/script.js
+++ javaScript/script.js
@@ -18,30 +18,3 @@ const car = {
     engine: ['2.0', '2.0T', '2.5', '2.5sti-best subaru engine']
 };
 console.log(car.engine[3])
-
-
-const fastCar = [
-    {
-        model: 'toyota',
-        brand: 'supra',
-        engine: ['2JZ-GTE', 'maxsPower 2033Hp']
-    }
-];
-for (let i = 0; i < fastCar.length; i++){
-    console.log(fastCar[i].engine[0])
-    console.log(fastCar[i].engine[1])
-};
-
-
-let vaxosAge = 50 - 34;
-
-console.log(vaxosAge);
-
-
-var giosAge = 49;
-var sabasAge = 19;
-var ladosAge = giosAge - sabasAge;
-
-console.log(ladosAge);
-
-console.log(Boolean(ladosAge > 15));
