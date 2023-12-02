b09e192eb1dfb89a899cdf7b2b75504cff2450a1
VAxoJan <vaxojaniashvili186@gmail.com>
2023-12-02 21:29:03 +0400

    homeweorks

---
+++ javaScript/homework-2/script.js
@@ -0,0 +1,17 @@
+const products = [
+    {
+        name: "Product X",
+        description: "A high-quality product with many features.",
+        price: 100
+    },
+    {
+        name: "Product Y",
+        description: "Another great product with a competitive price.",
+        price: 75
+    },
+    {
+        name: "Product Z",
+        description: "A basic product that gets the job done.",
+        price: 50
+    }
+];
---
+++ javaScript/homework-3/script.js
@@ -0,0 +1,8 @@
+const products = [
+    { name: "Laptop", price: 1299 },
+    { name: "Phone", price: 499 },
+    { name: "Tablet", price: 299 },
+    { name: "Watch", price: 199 },
+    { name: "Headphones", price: 99 },
+];
+let number = [];
