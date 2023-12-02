0ebca8d222620570c68626f5019103434f7a0ea7
VAxoJan <vaxojaniashvili186@gmail.com>
2023-12-02 16:57:02 +0400

    homework-1 finished

---
+++ javaScript/homework-1/script.js
@@ -0,0 +1,9 @@
+const productsFeatures = {
+    name: 'Product X',
+    description: 'A high-quality product with many features.',
+    price: 100,
+    color: 'red',
+    size: 'medium',
+    weight: 5
+};
+console.log(productsFeatures);
