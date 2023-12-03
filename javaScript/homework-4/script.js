2e8d55649918b3bc675896d0178c41712c4f0181
VAxoJan <vaxojaniashvili186@gmail.com>
2023-12-03 23:30:49 +0400

    tablet-almost-finished

--- javaScript/homework-4/script.js
+++ javaScript/homework-4/script.js
@@ -1,3 +1,14 @@
+function total(numbers) {
+    return numbers * 3;
+}
+
+
+
 const numbers = [33, 10, 99, 95];
 
-console.log(numbers.*3);
+const age = total(33, 10, 90, 95);
+
+
+
+
+console.log(numbers*3);
