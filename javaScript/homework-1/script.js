a971bbf83637c6e227bc55156c217e895dd680b4
VAxoJan <vaxojaniashvili186@gmail.com>
2023-12-06 18:27:54 +0400

    jS

--- javaScript/homework-1/script.js
+++
@@ -1,9 +0,0 @@
-const productsFeatures = {
-    name: 'Product X',
-    description: 'A high-quality product with many features.',
-    price: 100,
-    color: 'red',
-    size: 'medium',
-    weight: 5
-};
-console.log(productsFeatures);
---
+++ javaScript/script.js
@@ -0,0 +1,8 @@
+const box = document.querySelector('.box-1');
+
+console.log(box);
+
+for (const el of box) {
+    el.style.backgroundColor = 'yellow';
+}
+
