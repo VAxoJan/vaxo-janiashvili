8109fea180cadb42289ceababc378d09f9b8fc9b
VAxoJan <vaxojaniashvili186@gmail.com>
2023-12-12 02:20:25 +0400

    homework-1-finisihed-js

--- intro-component/script.js
+++ intro-component/script.js
@@ -7,21 +7,60 @@ const submit = document.querySelector('button');
 
 const validateFirstName = () => {
     const val = firstName.value;
+    const alert = document.querySelector('.email-alert');
+    alert.innerText = 'First Name cannot be empty';
+    alert.style.color = 'red';
+    alert.style.paddingLeft = '250px';
     if ((val.length > 3) || (val.length >= 3)) {
+        alert.innerText = '';
         firstName.style.borderColor = 'green';
     }
     else {
         firstName.style.borderColor = 'red';
-
     }
 }
 const validateLastName = () => {
     const val = lastName.value;
+    const alerti = document.querySelector('.email-alert-2');
+    alerti.innerText = 'Last Name cannot be empty';
+    alerti.style.color = 'red';
+    alerti.style.paddingLeft = '250px';
     if ((val.length > 3) || (val.length >= 3)) {
         lastName.style.borderColor = 'green'
+        alerti.innerText = '';
     }
     else (
         lastName.style.borderColor = 'red'
+
+    )
+}
+const validateEmail = () => {
+    const val = email.value;
+    const lok = document.querySelector('.looks');
+    lok.innerText = 'Looks like this is not an email';
+    lok.style.color = 'red';
+    lok.style.paddingLeft = '250px';
+    if ((val.length > 3) || (val.length >= 3) && (val.includes('@'))) {
+        lok.innerText = '';
+        email.style.borderColor = 'green'
+    }
+    else (
+        email.style.borderColor = 'red'
+
+    )
+}
+const validatePassword = () => {
+    const val = pass.value;
+    const loki = document.querySelector('.passwordi');
+    loki.innerText = 'Password cannot be empty';
+    loki.style.color = 'red';
+    loki.style.paddingLeft = '250px';
+    if ((val.length > 5) || (val.length >= 3)) {
+        pass.style.borderColor = 'green'
+        loki.innerText = '';
+    }
+    else (
+        pass.style.borderColor = 'red'
 
     )
 }
@@ -29,4 +68,6 @@ const validateLastName = () => {
 submit.addEventListener('click', ()  => {
     validateFirstName();
     validateLastName();
+    validateEmail();
+    validatePassword();
 })
