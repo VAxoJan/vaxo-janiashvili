506a8a6e79483f6b4584466d03b64982a040946d
VAxoJan <vaxojaniashvili186@gmail.com>
2023-12-16 12:42:01 +0400

    memgoni-ragaca-gamamdis

--- javaScript/script.js
+++ javaScript/script.js
@@ -1,69 +1,24 @@
 const add = document.querySelector('.add');
 const ul = document.querySelector('.ul');
-const li = document.querySelector('li');
 
-let counter = 0; 
-
+let counter = 0;
 
 const generateListItem = val => {
     const li = document.createElement('li');
     li.classList.add(`li-${counter++}`);
-    li.innerHTML = `${val} <button>delete</button>`;
+    li.innerHTML = `${val}  <img class="trashi" src="./images/akar-icons_trash-can.svg" alt="">`;
     ul.appendChild(li);
 }
 
- 
+ul.addEventListener('click', (event) => {
+    if (event.target.classList.contains('trashi')) {
+        const listItem = event.target.closest('li');
+        listItem.remove();
+    }
+});
+
 add.addEventListener('click', () => {
     const input = document.querySelector('input');
     generateListItem(input.value);
 });
 
-
-
-
-
-
-
-
-
-
-
-
-/*
-
-const add = document.querySelector('.add');
-const remove = document.querySelector('.remove');
-const ul = document.querySelector('ul');
-const rgoli = document.querySelector('.check');
-const chek = document.querySelector('.chek');
-const ed = document.querySelector('#add');
-
-remove.addEventListener('click', () => {
-    const vaxo = document.querySelector('li');
-    vaxo.remove();
-    console.log(remove.style.display = 'none');
-    console.log(rgoli.style.display = 'none');
-    console.log(chek.style.display = 'none');
-});
-
-
-add.addEventListener('click', () => {
-    const input = document.querySelector('input');
-    const li = document.createElement('li');
-    li.innerText = input.value;
-    ul.appendChild(li);
-    console.log(remove.style.display = 'inherit');
-    console.log(rgoli.style.display = 'inherit');
-    console.log(chek.style.display = 'inherit');
-    console.log(ed.style.paddingLeft = '34px');
-});
-
-
-rgoli.addEventListener('click', () => {
-    console.log(rgoli.style.backgroundColor = '#20EEB0') (rgoli.style.borderRadius = '50%');
-});
-
-rgoli.addEventListener('mouseover', () => {
-    console.log(rgoli.style.backgroundColor = '#fff');
-});
-*/
