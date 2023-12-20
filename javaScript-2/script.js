2105e22b86ec98664239ae918c82ddc05270090b
vaxo <vaxojaniashvili186@gmail.com>
2023-12-20 16:33:08 +0400

    working

--- javaScript-2/script.js
+++ javaScript-2/script.js
@@ -1,4 +1,4 @@
 const initData = async () => {
     const data = await fetch('./json.js').then(response => response.json());
-    console.log(data.title);
+    console.log(data.amount);
 }
--- test/script.js
+++ test/script.js
@@ -1,20 +1,61 @@
-// Parsing JSON
-var jsonString = '{"name": "John", "age": 30, "city": "New York"}';
-var jsonObj = JSON.parse(jsonString);
+const user = 20 < 10;
 
-console.log(jsonObj.name); // Output: John
-console.log(jsonObj.age);  // Output: 30
-console.log(jsonObj.city); // Output: New York
+const promise = new Promise ((resolve, reject) => {
+    if (user) {
+        resolve('zd dzmajan');
+    } 
+    else {
+            reject('au errori daartka simon');
+    }
+});
 
-// Creating JSON
-var person = { name: "John", age: 30, city: "New York" };
-var jsonString = JSON.stringify(person);
 
-console.log(jsonString);
-// Output: {"name":"John","age":30,"city":"New York"}
+promise.then((mess) => {
+    console.log(mess);
+}).catch((errorrrr) => {
+    console.log(errorrrr);
+})
 
-// Nested JSON
-var nestedJsonString = '{"person": {"name": "Alice", "age": 25}}';
-var nestedJsonObj = JSON.parse(nestedJsonString);
 
-console.log(nestedJsonObj.person.name); // Output: Alice
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// // Parsing JSON
+// var jsonString = '{"name": "John", "age": 30, "city": "New York"}';
+// var jsonObj = JSON.parse(jsonString);
+
+// console.log(jsonObj.name); // Output: John
+// console.log(jsonObj.age);  // Output: 30
+// console.log(jsonObj.city); // Output: New York
+
+// // Creating JSON
+// var person = { name: "John", age: 30, city: "New York" };
+// var jsonString = JSON.stringify(person);
+
+// console.log(jsonString);
+// // Output: {"name":"John","age":30,"city":"New York"}
+
+// // Nested JSON
+// var nestedJsonString = '{"person": {"name": "Alice", "age": 25}}';
+// var nestedJsonObj = JSON.parse(nestedJsonString);
+
+// console.log(nestedJsonObj.person.name); // Output: Alice
