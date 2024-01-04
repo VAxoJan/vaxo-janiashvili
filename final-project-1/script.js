3473061420af2800ef1511c649b634ce01f29604
vaxo <vaxojaniashvili186@gmail.com>
2024-01-05 01:13:29 +0400

    working

---
+++ final-project-1/script.js
@@ -0,0 +1,6 @@
+const secondBtn = document.querySelector('#secondBtn');
+
+secondBtn.addEventListener('click', () => {
+    console.log(secondBtn.style.backgroundColor = '#20EEB0');
+    console.log(secondBtn.style.borderLeft = '4px solid black')
+});
